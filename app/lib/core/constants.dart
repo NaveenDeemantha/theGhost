13950fd861f8bd778f420f46d453858fc20de6a5
Naveen Deemantha <naveenpunchihewa9@gmail.com>
2026-05-21 13:21:40 +0530

    apk version

--- app/lib/core/constants.dart
+++ app/lib/core/constants.dart
@@ -1,4 +1,4 @@
 class AppConstants {
   // Change this to your machine's local IP when testing on a real device
-  static const String baseUrl = 'http://10.0.2.2:3000/api';
+  static const String baseUrl = 'http://192.168.1.186:3000/api';
 }
